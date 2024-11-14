16d7faaa84d46b343b21221db199aa6abc494e83
drbrodix <adamrigely@pm.me>
2024-11-14 20:17:45 +0100

    Added GetDatapointValue telegram class

--- BaosMini/include/telegram/GetDatapointValue.hpp
+++ BaosMini/include/telegram/GetDatapointValue.hpp
@@ -1,18 +1,25 @@
-//#ifndef GET_DATAPOINT_VALUE_HPP
-//#define GET_DATAPOINT_VALUE_HPP
-//
-//#include "telegram/BaosTelegram.hpp"
-//
-//class SetDatapointValue : public BaosTelegram
-//{
-//public:
-//	SetDatapointValue(
-//        int datapointId,
-//        bool datapointValue);
-//	~SetDatapointValue();
-//
-//private:
-//
-//};
-//
-//#endif // GET_DATAPOINT_VALUE_HPP
+#ifndef GET_DATAPOINT_VALUE_HPP
+#define GET_DATAPOINT_VALUE_HPP
+
+#include "telegram/BaosTelegram.hpp"
+
+enum FILTER_CODES
+{
+        GetAllDatapointValues = 0x00,
+        GetOnlyValidDatapointValues = 0x01,
+        GetOnlyUpdatedDatapointValues = 0x02
+};
+
+class GetDatapointValue : public BaosTelegram
+{
+public:
+	GetDatapointValue(
+       int datapointId,
+       int nrOfDatapoints);
+	~GetDatapointValue();
+
+private:
+
+};
+
+#endif // GET_DATAPOINT_VALUE_HPP
--- BaosMini/src/telegram/GetDatapointValue.cpp
+++ BaosMini/src/telegram/GetDatapointValue.cpp
@@ -1,22 +1,18 @@
-//#include "telegram/SetDatapointValue.hpp"
-//
-//SetDatapointValue::SetDatapointValue(
-//    int datapointId,
-//    bool datapointValue)
-//{
-//    baosTelegram.push_back(BaosTelegram::BAOS_MAIN_SERVICE);
-//    baosTelegram.push_back(BaosSubServices::SetDatapointValueReq);
-//    baosTelegram.push_back(0x00);
-//    baosTelegram.push_back(unsigned char(datapointId)); // ID of first datapoint to set
-//    baosTelegram.push_back(0x00);
-//    baosTelegram.push_back(0x01); // Number of datapoints to set
-//    baosTelegram.push_back(0x00);
-//    baosTelegram.push_back(unsigned char(datapointId)); // ID of first datapoint
-//    baosTelegram.push_back(0x03); // Command byte of first datapoint - 0b0011 == Set value and send on bus
-//    baosTelegram.push_back(0x01); // Length byte of first datapoint
-//    baosTelegram.push_back(datapointValue); // Value of first datapoint
-//}
-//
-//SetDatapointValue::~SetDatapointValue()
-//{
-//}
+#include "telegram/GetDatapointValue.hpp"
+
+GetDatapointValue::GetDatapointValue(
+   int datapointId,
+   int nrOfDatapoints)
+{
+   baosTelegram.push_back(BaosTelegram::BAOS_MAIN_SERVICE);
+   baosTelegram.push_back(BaosSubServices::GetDatapointValueReq);
+   baosTelegram.push_back(0x00);
+   baosTelegram.push_back(unsigned char(datapointId)); // ID of first datapoint to set
+   baosTelegram.push_back(0x00);
+   baosTelegram.push_back(unsigned char(nrOfDatapoints)); // Maximum number of datapoints to get
+   baosTelegram.push_back(FILTER_CODES::GetAllDatapointValues); // Filter, which datapoints should be retrieved
+}
+
+GetDatapointValue::~GetDatapointValue()
+{
+}
